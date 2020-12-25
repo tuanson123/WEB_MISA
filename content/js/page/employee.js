d02ab18248e9b23fb112fa393e3630d14d48ba6a
Tuấn sơn Đõ <ankhoainang@gmail.com>
2020-12-25 15:14:36 +0700

    update

---
+++ content/js/page/employee.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    loadData();
+})
+function loadData() {
+    //Lấy dữ liệu
+    $.ajax(
+        {
+            url:"http://api.manhnv.net/api/employees",
+            method:"GET",
+        }
+    )
+        .done(function (res) {
+          
+            var data = res;
+           
+            $.each(data, function (index, item) {
+                var tr = $(`<tr>
+                        <td>`+ item['EmployeeCode'] +`</td>
+                        <td>`+ item['FullName'] +`</td>
+                        <td>`+ item['WorkStatus'] +`</td>
+                        <td>`+ item['JoinDate'] +`</td>
+                        <td>`+ item['PhoneNumber'] +`</td>
+                        <td>`+ item['Address'] +`</td>
+                        
+                    </tr >`);
+                $('table tbody').append(tr);
+                debugger;
+            })
+        })
+        .fail(function (res) { })
+    //binding dữ liệu
+}
